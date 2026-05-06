422d4964dc84213d87bb7de243aa58ec283d4315
Shaurya Chaudhary <shauryachaudhary2003@gmail.com>
2026-05-06 16:50:23 +0530

    chore: clean Vite starter, setup frontend structure and configure CORS

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const cors = require("cors");
+
+app.use(cors());
 
 const app = express();
-
 app.use(express.json());
 
 // Test route
