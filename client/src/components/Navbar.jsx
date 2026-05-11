b9dfd9e3721d89729ba325a9e6ff1fd9a2dc7a28
Shaurya Chaudhary <shauryachaudhary2003@gmail.com>
2026-05-11 17:44:24 +0530

    feat: add Login and Signup links to Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,18 @@
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 function Navbar() {
   return (
     <header className="navbar">
       <div className="container">
+        <Link to="/">
         <h1>Fitness Tracker</h1>
+        </Link>
+
+        <nav>
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Signup</Link>
+        </nav>
       </div>
     </header>
   );
