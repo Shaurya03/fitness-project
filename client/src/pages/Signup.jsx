0a3893996e75c1063c008986376f51a2058dc136
Shaurya Chaudhary <shauryachaudhary2003@gmail.com>
2026-05-11 19:39:20 +0530

    feat: create Signup form with state management and submit handling

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,8 +1,36 @@
+import { useState } from "react";
+import "./Signup.css";
+
 function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    console.log(email, password);
+  };
+
   return (
-    <div className="signup">
+    <form className="signup" onSubmit={handleSubmit}>
       <h2>Signup</h2>
-    </div>
+
+      <label>Email:</label>
+      <input
+        type="email"
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
+      />
+
+      <label>Password:</label>
+      <input
+        type="password"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+      />
+
+      <button type="submit">Signup</button>
+    </form>
   );
 }
 
