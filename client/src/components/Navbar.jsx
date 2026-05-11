5d3052813300af305a27cc99cac3308344de6179
Shaurya Chaudhary <shauryachaudhary2003@gmail.com>
2026-05-11 16:25:13 +0530

    feat: add Navbar component with styling for improved layout

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <header className="navbar">
+      <div className="container">
+        <h1>Fitness Tracker</h1>
+      </div>
+    </header>
+  );
+}
+
+export default Navbar;
