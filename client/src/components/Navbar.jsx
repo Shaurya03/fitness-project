a608997839b57985644518462fd14807f405f8c8
Shaurya Chaudhary <shauryachaudhary2003@gmail.com>
2026-05-13 17:47:34 +0530

    feat: add conditional navbar rendering and logout functionality

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,17 +1,34 @@
 import { Link } from 'react-router-dom';
+import { useAuthContext } from '../hooks/useAuthContext';
 import './Navbar.css';
 
 function Navbar() {
+  const { user, dispatch } = useAuthContext();
+
+  const handleLogout = () => {
+    localStorage.removeItem("user");
+    dispatch({ type: "LOGOUT" });
+  };
+
   return (
     <header className="navbar">
       <div className="container">
         <Link to="/">
-        <h1>Fitness Tracker</h1>
+          <h1>Fitness Tracker</h1>
         </Link>
 
         <nav>
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Signup</Link>
+          {user ? (
+            <div>
+              <span>{user.email}</span>
+              <button onClick={handleLogout}>Logout</button>
+            </div>
+          ) : (
+            <div>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Signup</Link>
+            </div>
+          )}
         </nav>
       </div>
     </header>
