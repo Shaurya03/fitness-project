8d7daf79b4a9aa2b93059c151150e5883dead0ba
Shaurya Chaudhary <shauryachaudhary2003@gmail.com>
2026-05-14 17:08:15 +0530

    feat: implement user-based routing for Home, Login, and Signup pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,30 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom';
+import { useAuthContext } from './hooks/useAuthContext';
+import { Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import Login from './pages/Login';
-import Signup from './pages/Signup';  
+import Signup from './pages/Signup';
 
 function App() {
+  const { user } = useAuthContext();
+
   return (
     <>
       <Navbar />
 
       <div className="pages">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+          <Route
+            path="/"
+            element={user ? <Home /> : <Navigate to="/login" />} />
+          <Route
+            path="/login"
+            element={!user ? <Login /> : <Navigate to="/" />} />
+          <Route
+            path="/signup"
+            element={!user ? <Signup /> : <Navigate to="/" />} />
         </Routes>
       </div>
     </>
